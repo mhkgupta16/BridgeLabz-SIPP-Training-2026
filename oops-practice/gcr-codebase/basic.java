57e459380144fa22318a1f4bf2c8230f00d1ec69
MhkGHub <guptamahak383@gmail.com>
2026-06-26 12:15:45 +0530

    Remove oops-practice from core-java-practice branch

--- oops-practice/gcr-codebase/basic.java
+++
@@ -1,6 +0,0 @@
-//final use in 3 places(variable,class,object)
-/*
-in polymorphism
-overloading-compile time error 
-in overriding- return type either theyr should be same or co variant(measn parent child relationship
-in overloading-return type doesn't matter*/
