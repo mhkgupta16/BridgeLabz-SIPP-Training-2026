b127d6e1bacf6b3e1f4907790c1d269f302a81a8
MhkGHub <guptamahak383@gmail.com>
2026-06-27 00:23:37 +0530

    [mahak] added

---
+++ oops-practice/gcr-codebase/Inheritance.java
@@ -0,0 +1,14 @@
+class Animal{
+  void walk(){
+    System.out.println("Animal Walk()");
+  }
+}
+class Monkey extends Animal{
+
+}
+
+public class Inheritance{
+  public static void main(String[] args){
+    Animal obj1= new Animal();
+  }
+}
